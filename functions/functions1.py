fc56e33aa2cdfab04e253b2d2151c0f0950ec582
soma sekhar <eaglesekhar03@gmail.com>
2021-05-07 17:04:22 +0530

    added functions

---
+++ functions/functions1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  7 17:02:47 2021
+
+@author: eagle
+"""
+
+######## functions #########
+
+def function1():
+    print("Hello world")
+    
+function1()
