37065161049b803eea0a9086b5beab8cdd997d7d
somasekhar1287 <eaglesekhar03@gmail.com>
2021-05-17 16:02:50 +0530

    added

--- Opencv/video.py
+++ Opencv/video.py
@@ -8,9 +8,18 @@ Created on Sun May  9 23:00:32 2021
 import cv2 as cv
 
 try:
-    capture=cv.VideoCapture("C:/Users/eagle/Videos/codbops2.mp4")
+    def rescale(frame,scale=0.75):
+       width=int(frame.shape[1]*scale)
+       height=int(frame.shape[0]*scale)
+       dims=(width,height)
+       return cv.resize(frame,dims,interpolation=cv.INTER_AREA)
+        
+        
+    capture=cv.VideoCapture("C:/Users/eagle/Videos/vtest.avi")
     while True:
         isTrue,frame=capture.read()
+        resize_frame=rescale(frame,0.25)
+        cv.imshow("Resize Video",resize_frame)
         cv.imshow("Video",frame)
         if cv.waitKey(25) & 0xFF==ord('q'):
             break
