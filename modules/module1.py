aa5faf9609468e6cf8969b48142291defee20f56
soma sekhar <eaglesekhar03@gmail.com>
2021-05-18 17:38:58 +0530

    added modules

---
+++ modules/module1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 18 16:06:22 2021
+
+@author: eagle
+"""
+
+
+
+import square
+
+li=[1,2,3,4,5,6,7]
+
+for i in li:
+    print(square.Square(i))
---
+++ modules/square.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 18 14:35:36 2021
+
+@author: eagle
+"""
+def Square(num):
+    return num*num
+
+
